a0c25feed9e198d7f927755f3c38605a09c4a933
baran <349355628@gapps.yrdsb.ca>
2025-01-11 19:47:32 -0500

    Created project

--- UNO - ISU/src/Main.java
+++ UNO - ISU/src/Main.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        String[] cards = {"0R", "0R", "1R", "2R", "3R", "4R", "5R", "6R", "7R", "8R", "9R", "Draw 2 R", "Draw 2 R",
+                "Skip R", "Skip R", "0Y", "0Y","1Y", "2Y", "3Y", "4Y", "5Y", "6Y", "7Y", "8Y", "9Y", "Draw 2 Y",
+                "Draw 2 Y", "Skip Y", "Skip Y", "0B", "0B", "1B", "2B", "3B", "4B", "5B", "6B", "7B", "8B", "9B", "Draw 2 B", "Draw 2 B",
+                "Skip B", "Skip B", "0G", "0G", "1G", "2G", "3G", "4G", "5G", "6G", "7G", "8G", "9G", "Draw 2 G", "Draw 2 G", "Skip G", "Skip G",
+                "Wild", "Wild", "Wild", "Wild", "Wild Draw 4", "Wild Draw 4", "Wild Draw 4", "Wild Draw 4"};
+        int currentIndex = cards.length;
+        int deck = 0;
+        for (int i = 0; i < currentIndex; i++) {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
         }
     }
 }
