7d4a77a1589e1e9356c2243bac77f3077337b52d
baran <349355628@gapps.yrdsb.ca>
2025-01-13 11:04:06 -0500

    Shuffled deck

--- UNO - ISU/src/Main.java
+++ UNO - ISU/src/Main.java
@@ -2,15 +2,113 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] cards = {"0R", "0R", "1R", "2R", "3R", "4R", "5R", "6R", "7R", "8R", "9R", "Draw 2 R", "Draw 2 R",
-                "Skip R", "Skip R", "0Y", "0Y","1Y", "2Y", "3Y", "4Y", "5Y", "6Y", "7Y", "8Y", "9Y", "Draw 2 Y",
-                "Draw 2 Y", "Skip Y", "Skip Y", "0B", "0B", "1B", "2B", "3B", "4B", "5B", "6B", "7B", "8B", "9B", "Draw 2 B", "Draw 2 B",
-                "Skip B", "Skip B", "0G", "0G", "1G", "2G", "3G", "4G", "5G", "6G", "7G", "8G", "9G", "Draw 2 G", "Draw 2 G", "Skip G", "Skip G",
-                "Wild", "Wild", "Wild", "Wild", "Wild Draw 4", "Wild Draw 4", "Wild Draw 4", "Wild Draw 4"};
-        int currentIndex = cards.length;
-        int deck = 0;
-        for (int i = 0; i < currentIndex; i++) {
-
+        ArrayList<String> shuffledDeck = new ArrayList<String>();
+        ArrayList<String> deck = new ArrayList<String>();
+        deck.add("0R");
+        deck.add("1R");
+        deck.add("1R");
+        deck.add("2R");
+        deck.add("2R");
+        deck.add("3R");
+        deck.add("3R");
+        deck.add("4R");
+        deck.add("4R");
+        deck.add("5R");
+        deck.add("5R");
+        deck.add("6R");
+        deck.add("6R");
+        deck.add("7R");
+        deck.add("7R");
+        deck.add("8R");
+        deck.add("8R");
+        deck.add("9R");
+        deck.add("9R");
+        deck.add("Draw 2 R");
+        deck.add("Draw 2 R");
+        deck.add("Skip R");
+        deck.add("Skip R");
+        deck.add("0B");
+        deck.add("1B");
+        deck.add("1B");
+        deck.add("2B");
+        deck.add("2B");
+        deck.add("3B");
+        deck.add("3B");
+        deck.add("4B");
+        deck.add("4B");
+        deck.add("5B");
+        deck.add("5B");
+        deck.add("6B");
+        deck.add("6B");
+        deck.add("7B");
+        deck.add("7B");
+        deck.add("8B");
+        deck.add("8B");
+        deck.add("9B");
+        deck.add("9B");
+        deck.add("Draw 2 B");
+        deck.add("Draw 2 B");
+        deck.add("Skip B");
+        deck.add("Skip B");
+        deck.add("0G");
+        deck.add("1G");
+        deck.add("1G");
+        deck.add("2G");
+        deck.add("2G");
+        deck.add("3G");
+        deck.add("3G");
+        deck.add("4G");
+        deck.add("4G");
+        deck.add("5G");
+        deck.add("5G");
+        deck.add("6G");
+        deck.add("6G");
+        deck.add("7G");
+        deck.add("7G");
+        deck.add("8G");
+        deck.add("8G");
+        deck.add("9G");
+        deck.add("9G");
+        deck.add("Draw 2 G");
+        deck.add("Draw 2 G");
+        deck.add("Skip G");
+        deck.add("Skip G");
+        deck.add("0Y");
+        deck.add("1Y");
+        deck.add("1Y");
+        deck.add("2Y");
+        deck.add("2Y");
+        deck.add("3Y");
+        deck.add("3Y");
+        deck.add("4Y");
+        deck.add("4Y");
+        deck.add("5Y");
+        deck.add("5Y");
+        deck.add("6Y");
+        deck.add("6Y");
+        deck.add("7Y");
+        deck.add("7Y");
+        deck.add("8Y");
+        deck.add("8Y");
+        deck.add("9Y");
+        deck.add("9Y");
+        deck.add("Draw 2 Y");
+        deck.add("Draw 2 Y");
+        deck.add("Skip Y");
+        deck.add("Skip Y");
+        deck.add("Wild Card");
+        deck.add("Wild Card");
+        deck.add("Wild Card");
+        deck.add("Wild Card");
+        deck.add("Wild Draw 4");
+        deck.add("Wild Draw 4");
+        deck.add("Wild Draw 4");
+        deck.add("Wild Draw 4");
+        for (int i = 0; i < deck.toArray().length; i++) {
+            i = (int)(Math.random()* (100 - 1 + 1) + 1);
+            String newCard = deck.get(i);
+            shuffledDeck.add(newCard);
         }
+        System.out.println(shuffledDeck);
     }
 }
---
+++ UNO - ISU/src/main2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] cards = {"0R", "0R", "1R", "2R", "3R", "4R", "5R", "6R", "7R", "8R", "9R", "Draw 2 R", "Draw 2 R",
+                "Skip R", "Skip R", "0Y", "0Y","1Y", "2Y", "3Y", "4Y", "5Y", "6Y", "7Y", "8Y", "9Y", "Draw 2 Y",
+                "Draw 2 Y", "Skip Y", "Skip Y", "0B", "0B", "1B", "2B", "3B", "4B", "5B", "6B", "7B", "8B", "9B", "Draw 2 B", "Draw 2 B",
+                "Skip B", "Skip B", "0G", "0G", "1G", "2G", "3G", "4G", "5G", "6G", "7G", "8G", "9G", "Draw 2 G", "Draw 2 G", "Skip G", "Skip G",
+                "Wild", "Wild", "Wild", "Wild", "Wild Draw 4", "Wild Draw 4", "Wild Draw 4", "Wild Draw 4"};
+        int currentIndex = cards.length;
+        int deck = 0;
+        for (int i = 0; i < currentIndex; i++) {
+
+        }
+    }
+}
