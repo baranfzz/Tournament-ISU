5c6d6e08baabba7ef5c9bf2e0b01d626f9b682f6
baran <349355628@gapps.yrdsb.ca>
2025-01-16 22:52:54 -0500

    Restarted: Tournament ISU - Initialised team names - Tested for Uniqueness - Generated two groups - Randomised rounds

--- UNO - ISU/src/Main.java
+++ UNO - ISU/src/Main.java
@@ -2,144 +2,35 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> shuffledDeck = new ArrayList<String>();
-        ArrayList<String> deck = new ArrayList<String>();
-        ArrayList<String> mainDeck = new ArrayList<String>();
-        ArrayList<String> player1 = new ArrayList<String>();
-        ArrayList<String> player2 = new ArrayList<String>();
-        deck.add("0R");
-        deck.add("1R");
-        deck.add("1R");
-        deck.add("2R");
-        deck.add("2R");
-        deck.add("3R");
-        deck.add("3R");
-        deck.add("4R");
-        deck.add("4R");
-        deck.add("5R");
-        deck.add("5R");
-        deck.add("6R");
-        deck.add("6R");
-        deck.add("7R");
-        deck.add("7R");
-        deck.add("8R");
-        deck.add("8R");
-        deck.add("9R");
-        deck.add("9R");
-        deck.add("Draw 2 R");
-        deck.add("Draw 2 R");
-        deck.add("Skip R");
-        deck.add("Skip R");
-        deck.add("0B");
-        deck.add("1B");
-        deck.add("1B");
-        deck.add("2B");
-        deck.add("2B");
-        deck.add("3B");
-        deck.add("3B");
-        deck.add("4B");
-        deck.add("4B");
-        deck.add("5B");
-        deck.add("5B");
-        deck.add("6B");
-        deck.add("6B");
-        deck.add("7B");
-        deck.add("7B");
-        deck.add("8B");
-        deck.add("8B");
-        deck.add("9B");
-        deck.add("9B");
-        deck.add("Draw 2 B");
-        deck.add("Draw 2 B");
-        deck.add("Skip B");
-        deck.add("Skip B");
-        deck.add("0G");
-        deck.add("1G");
-        deck.add("1G");
-        deck.add("2G");
-        deck.add("2G");
-        deck.add("3G");
-        deck.add("3G");
-        deck.add("4G");
-        deck.add("4G");
-        deck.add("5G");
-        deck.add("5G");
-        deck.add("6G");
-        deck.add("6G");
-        deck.add("7G");
-        deck.add("7G");
-        deck.add("8G");
-        deck.add("8G");
-        deck.add("9G");
-        deck.add("9G");
-        deck.add("Draw 2 G");
-        deck.add("Draw 2 G");
-        deck.add("Skip G");
-        deck.add("Skip G");
-        deck.add("0Y");
-        deck.add("1Y");
-        deck.add("1Y");
-        deck.add("2Y");
-        deck.add("2Y");
-        deck.add("3Y");
-        deck.add("3Y");
-        deck.add("4Y");
-        deck.add("4Y");
-        deck.add("5Y");
-        deck.add("5Y");
-        deck.add("6Y");
-        deck.add("6Y");
-        deck.add("7Y");
-        deck.add("7Y");
-        deck.add("8Y");
-        deck.add("8Y");
-        deck.add("9Y");
-        deck.add("9Y");
-        deck.add("Draw 2 Y");
-        deck.add("Draw 2 Y");
-        deck.add("Skip Y");
-        deck.add("Skip Y");
-        deck.add("Wild Card");
-        deck.add("Wild Card");
-        deck.add("Wild Card");
-        deck.add("Wild Card");
-        deck.add("Wild Draw 4");
-        deck.add("Wild Draw 4");
-        deck.add("Wild Draw 4");
-        deck.add("Wild Draw 4");
-        for (int i = 0; i < deck.toArray().length; i++) {
-            i = (int) (Math.random() * (100 - 1 + 1) + 1);
-            String newCard = deck.get(i);
-            shuffledDeck.add(newCard);
-        }
-        for (int i = 0; i < 7; i++) {
-            shuffledDeck.remove(i);
-            player1.add(shuffledDeck.get(i));
-        }
-        for (int i = 0; i < 7; i++) {
-            shuffledDeck.remove(i);
-            player2.add(shuffledDeck.get(i));
-        }
-        mainDeck.add(shuffledDeck.get(shuffledDeck.size() - 1));
-        if (player1.contains(String.valueOf((mainDeck.get(mainDeck.size() - 1)).charAt(0))) || player1.contains(String.valueOf((shuffledDeck.get(shuffledDeck.size() - 1)).charAt(1)))) {
-            System.out.println("What card would you like to place?");
-            String player1Card = sc.nextLine();
-            player1.remove(player1.indexOf(player1Card));
-            mainDeck.add(player1Card);
+        ArrayList<String> teams = new ArrayList<String>();
+        ArrayList<String> group1 = new ArrayList<String>();
+        ArrayList<String> group2 = new ArrayList<String>();
+        String round1 = " ";
+        System.out.println("Enter the names of your teams:");
+        for (int i = 0; i < 8; i++) {
+            String name = sc.nextLine();
+            if (!teams.contains(name)) {
+                teams.add(name);
+                System.out.println("All team names are unique, you may proceed.");
+                System.out.println("Here are the teams:" + teams);
+            } else {
+                System.out.println("Team already exists, please try again.");
+                sc.nextLine();
             }
-        else {
-            player1.add(shuffledDeck.get(shuffledDeck.size() - 1));
-            shuffledDeck.remove(shuffledDeck.size() - 1);
         }
-        if (player2.contains(String.valueOf((mainDeck.get(mainDeck.size() - 1)).charAt(0))) || player2.contains(String.valueOf((shuffledDeck.get(shuffledDeck.size() - 1)).charAt(1)))) {
-            System.out.println("What card would you like to place?");
-            String player2Card = sc.nextLine();
-            player2.remove(player2.indexOf(player2Card));
-            mainDeck.add(player2Card);
+        for (int i = 0; i < 4; i++) {
+            i = (int) (Math.random() * (7 + 1) + 0);
+            String member = teams.get(i);
+            teams.remove(i);
+            group1.add(member);
         }
-        else {
-            player2.add(shuffledDeck.get(shuffledDeck.size() - 1));
-            shuffledDeck.remove(shuffledDeck.size() - 1);
+        group2 = teams;
+        System.out.println(group1);
+        System.out.println(group2);
+        for (int i = 0; i < 4; i++) {
+            for (int j = i + 1; j < 4; j++) {
+                System.out.println(group1.get(i) + " will play against " + group1.get(j));
+            }
         }
     }
 }
